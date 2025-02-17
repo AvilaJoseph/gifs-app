7d52037388b84357e64526cbaaad9f1f39a5c1ca
AvilaJoseph <avilajoseph2021@gmail.com>
2025-02-16 23:22:05 -0500

    Creación del archivo de servicio de peticiones

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomePageComponent } from './services/home/home.component';
+import { HomePageComponent } from './pages/home/home.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
 import { CardListComponent } from './components/card-list/card-list.component';
 
